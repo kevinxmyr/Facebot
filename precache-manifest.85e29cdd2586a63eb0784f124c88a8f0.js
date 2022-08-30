61bdc0a3b0c026f31346b1a1ad7b46d929fc4239
kevinxmyr <109541263+kevinxmyr@users.noreply.github.com>
2022-08-30 13:50:18 +0800

    Updates

---
+++ precache-manifest.85e29cdd2586a63eb0784f124c88a8f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c717b89a566ea72f63f2",
+    "url": "/Facebot/static/css/main.7a72e5e2.chunk.css"
+  },
+  {
+    "revision": "c717b89a566ea72f63f2",
+    "url": "/Facebot/static/js/main.c717b89a.chunk.js"
+  },
+  {
+    "revision": "af5c29d1fa7150adfd2c",
+    "url": "/Facebot/static/css/1.dc952d8a.chunk.css"
+  },
+  {
+    "revision": "af5c29d1fa7150adfd2c",
+    "url": "/Facebot/static/js/1.af5c29d1.chunk.js"
+  },
+  {
+    "revision": "364b84866d4614d2748e",
+    "url": "/Facebot/static/js/runtime~main.364b8486.js"
+  },
+  {
+    "revision": "5678d19f31c8f1532235be4e43533d55",
+    "url": "/Facebot/index.html"
+  }
+];
